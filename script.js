efc83f8a6c34b2c089d174e29608b4d3f22b4b8d
rohanssharrmaa <nbnvz9d7cw@privaterelay.appleid.com>
2026-07-29 01:45:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
+function changeScene(hide, show){
 
+document.getElementById(hide)
+.classList.add("hidden");
+
+
+document.getElementById(show)
+.classList.remove("hidden");
+
+}
+
+
+
+function openHeart(){
+
+changeScene(
+"rohanScene",
+"heartScene"
+);
+
+}
+
+
+
+function showDisha(){
+
+changeScene(
+"heartScene",
+"dishaScene"
+);
+
+}
+
+
+
+function moonScene(){
+
+changeScene(
+"dishaScene",
+"moonScene"
+);
+
+}
+
+
+
+function ending(){
+
+changeScene(
+"moonScene",
+"ending"
+);
+
+}
+
+
+
+// create petals
+
+for(let i=0;i<40;i++){
+
+let petal=document.createElement("div");
+
+petal.innerHTML="🌸";
+
+petal.className="petal";
+
+petal.style.left=Math.random()*100+"%";
+
+petal.style.animationDelay=
+Math.random()*10+"s";
+
+
+document.querySelector(".petals")
+.appendChild(petal);
+
+}
